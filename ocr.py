040f5752e165d02001edca9b5ce988da22d6131a
oneshell <mitchels.umd@gmail.com>
2017-07-28 18:03:29 -0400

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -5,7 +5,7 @@
 
 # Workflow:
 # 1. Read image
-# 2. Edge detection
+# 2. Grayscale, blur, & edge detection
 # 3. Use detected edges to identify region of image containing text
 # 4. Implement OCR to identify text
 
@@ -18,6 +18,7 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/te
 import cv2
 import os
 import imutils
+import numpy as np
 
 print('Import successful' + '\n')
 
@@ -25,10 +26,10 @@ print('Import successful' + '\n')
 
 # Initialize variables describing anticipated sign type
 # will use these descriptions for number of contour points
-stop = 8
-license = 4
-speed = 4
-outlet = 4
+stop_sign = 8
+license_plate = 4
+speed_sign = 4
+outlet_sign = 4
 yield_sign = 3
 
 ####################################################
@@ -40,9 +41,15 @@ cv2.imshow("Original", image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", gray)
 
+# Blur image to decrease noise
+gray = cv2.medianBlur(gray, 3)
+cv2.imshow("Blurred", gray)
+
 # Find edges
 edged = cv2.Canny(gray, 30, 200)
 cv2.imshow("Edged", edged)
+
+####################################################
 
 # Find contours in the edged image, keep only the largest
 # ones, and initialize our screen contour
@@ -58,12 +65,29 @@ for c in cnts:
 
     # If our approximated contour has x-points, then
     # we can assume that we have found our screen
-    if len(approx) == outlet:
+    if len(approx) == outlet_sign:
         screenCnt = approx
         break
 
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
 cv2.imshow("Countours", image)
+
+####################################################
+
+# create polygon (trapezoid) mask to select region of interest
+mask = np.zeros((image.shape[0], image.shape[1]), dtype="uint8")
+pts = np.array([screenCnt], dtype=np.int32)
+print(len(screenCnt))
+print(screenCnt)
+print(screenCnt.shape)
+# cv2.fillConvexPoly(mask, pts, 255)
+# cv2.imshow("Mask", mask)
+#
+# # apply mask and show masked image on screen
+# masked = cv2.bitwise_and(image, image, mask=mask)
+# cv2.imshow("Region of Interest", masked)
+
+####################################################
 
 # Threshold image
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
@@ -72,10 +96,6 @@ cv2.imshow("Threshold", gray)
 # Invert image if desired
 gray = cv2.bitwise_not(gray)
 cv2.imshow("Flipped", gray)
-
-# Blur image to decrease noise
-gray = cv2.medianBlur(gray, 3)
-cv2.imshow("Blurred", gray)
 
 # Create temporary image to run OCR
 imagename = "{}.png".format(os.getpid())
